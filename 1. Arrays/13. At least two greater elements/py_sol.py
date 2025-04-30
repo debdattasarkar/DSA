ebe1d73aac5414ea59c5f6f7bb5fb50a6c7a411a
Debdatta Sarkar <debdattasmail@gmail.com>
2025-04-30 21:18:52 +0530

    updates

---
+++ 1. Arrays/13. At least two greater elements/py_sol.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+
+class Solution:
+    def findElements(self,arr):
+        # Your code goes here
+        # Step 1: Sort the array
+        arr.sort()  # O(n log n)
+    
+        # Step 2: Return all elements except the last two (which are the greatest)
+        return arr[:-2]  # O(n)
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while (T > 0):
+
+        arr = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(*ob.findElements(arr))
+
+        T -= 1
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
